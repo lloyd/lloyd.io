f4cc011a413843929f41daef8d185bbf18a1c796
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-06-06 07:37:15 -0600

    bring in all me posts

---
+++ scripts/genix.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+#
+# generate a blog index and shit.  It\'ll look like this
+#
+# <div id="posts">
+#   <div class="post" filename="2011-06-02-jsonselect-grows-up.html"
+#        shortname="jsonselect-grows-up">
+#     <div class="title">JSONSelect Grows Up</div>
+#     <div class="date">2011.06.02</div> 
+#     <div class="abstract">
+#       <a href="http://jsonselect.org/">JSONSelect</a> is a query language
+#       for JSON.  With JSONSelect you can write small patterns that match
+#       against JSON documents.  The language is mostly an adaptation of
+#       CSS to JSON, motivated by the belief that CSS does a fine job and
+#       is widely understood.</p>
+#     </div>
+#   </div>
+# </div>
+
+console.log("write me");
