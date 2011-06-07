041b242393205b49b2ee4cbb5db180a0d29d6008
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-06-07 05:23:21 -0600

    style fixes for three latest posts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
+function doRoute() {
+    var pn = window.location.pathname;
+    if (pn === '/index.html' || pn === '/') {
+        $("#posts").fadeIn(700);
+    } else {
+        // find the post we're talking about
+        var p = $(".post[shortname=\"" + pn.substr(1) + "\"]");
+        if (p.length === 0) {
+            // 404
+            alert("404: " + pn);
+            return;
+        }
+        p = p[0];
+        console.log("getting");
+        $.get("posts/" + $(p).attr('filename'), function(data) {
+            data = data.replace(/^title:.*$/m, '');
+            console.log("got", data);
+            $("#content .title").text($(p).find(".title a").text());
+            $("#content .date").text($(p).find(".date").text());
+            $("#content .post").html(data);
+            $("#content").fadeIn(700);
+            $('pre code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
+        });
+    }
+}
+
 $(document).ready(function() {
     $.get("posts/summary.html", function(data) {
         $("#posts").html(data);
+
+        doRoute();
     });
 });
